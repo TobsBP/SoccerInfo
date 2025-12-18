87a30bd10d5ab76b3b0ce35e69b867df8d883fee
Tobias <tobias.bp2004@gmail.com>
2025-12-18 14:03:08 -0300

    feat: team schema and interface updated

--- src/types/Interfaces/ITeam.ts
+++ src/types/Interfaces/ITeam.ts
@@ -2,4 +2,22 @@ export interface Team {
   id: number;
   name: string;
   logoUrl: string | null;
+  foundedYear?: number | null;
+  stadium?: string | null;
+  city?: string | null;
+  state?: string | null;
+  country: string;
+  titlesTotal: number;
+  titlesLeague: number;
+  titlesCup: number;
+  titlesIntl: number;
+  colors?: string | null;
+  website?: string | null;
+  socialMedia?: string | null;
+}
+
+export interface TeamDetailsPageProps {
+  params: Promise<{
+    id: string;
+  }>;
 }
